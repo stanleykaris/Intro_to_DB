82298d43786967a6f7ef02379ff9dfc4aaf0fdcd
Stanley Kariuki <stanleykariuki1999@gmail.com>
2025-02-02 22:13:53 +0300

    Add initial database setup and configuration for Bookstore application

---
+++ Bookstore Alx Connection.session.sql
@@ -0,0 +1,48 @@
+---We are going to create a database
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+USE alx_book_store;
+
+--We shall start by creating a table for books
+CREATE TABLE Books (
+book_id INT AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(130) NOT NULL,
+author_id INT,
+price DOUBLE NOT NULL,
+publication_date DATE,
+FOREIGN KEY (author_id) REFERENCES Authors (author_id) ON DELETE SET NULL
+);
+
+--We create table for Authors
+
+CREATE TABLE Authors(
+author_id INT AUTO_INCREMENT PRIMARY KEY,
+author_name VARCHAR(215) NOT NULL
+);
+
+--We create table for Customers
+
+CREATE TABLE Customers(
+customer_id INT AUTO_INCREMENT PRIMARY KEY,
+customer_name VARCHAR(215) NOT NULL,
+email VARCHAR(215) UNIQUE NOT NULL,
+address TEXT    
+);
+
+--Create table for Orders
+CREATE TABLE Orders(
+order_id INT AUTO_INCREMENT PRIMARY KEY,
+customer_id INT,
+order_date DATE NOT NULL,
+FOREIGN KEY (customer_id) REFERENCES Customers(customer_id) ON DELETE CASCADE   
+);
+
+--Create table for Order details
+
+CREATE TABLE Order_Details(
+orderdetail_id INT AUTO_INCREMENT PRIMARY KEY,
+order_id INT,
+book_id INT,
+quantity DOUBLE  NOT NULL CHECK (quantity > 0),
+FOREIGN KEY (order_id) REFERENCES Orders(order_id) ON DELETE CASCADE,
+FOREIGN KEY (book_id) REFERENCES Books(book_id) ON DELETE CASCADE
+);
